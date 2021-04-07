cc302cdcd82378e90cd64485e564a1431e985395
Arda Serdar Pektezol <info@ardapektezol.com>
2021-04-07 17:24:57 +0300

    update

--- showStats.js
+++ showStats.js
@@ -1,3 +1,6 @@
+if (navigator.serviceWorker) {
+		navigator.serviceWorker.register('/p2VerifierLeaderboard/sw.js', {scope: '/p2VerifierLeaderboard/'})
+	}
 var txtInput = fetch('output.txt')
   .then(response => response.text())
   .then(text => document.getElementById("table").innerHTML = text)
