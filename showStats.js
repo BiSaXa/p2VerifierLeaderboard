0313d6d2b8d08b7cd9a7d6a78dcaae082f157f3e
Arda Serdar Pektezol <info@ardapektezol.com>
2021-04-07 20:10:44 +0300

    Update showStats.js

--- showStats.js
+++ showStats.js
@@ -1,7 +1,7 @@
 if (navigator.serviceWorker) {
 		navigator.serviceWorker.register('/p2VerifierLeaderboard/sw.js', {scope: '/p2VerifierLeaderboard/'})
 }
-var mainText = ""
+/*var mainText = ""
 function setText(url) {
         var doc = new XMLHttpRequest();
         doc.onreadystatechange = function() {
@@ -13,11 +13,20 @@ function setText(url) {
         doc.setRequestHeader("Content-Encoding", "UTF-8");
         doc.send();
     }
-setText("https://bisaxa.github.io/p2VerifierLeaderboardBackend/")
-//let xhr = new XMLHttpRequest();
-//xhr.open("GET", "https://bisaxa.github.io/p2VerifierLeaderboardBackend/" + ((/\?/).test("https://bisaxa.github.io/p2VerifierLeaderboardBackend/") ? "&" : "?") + (new Date()).getTime(), false);
-//xhr.send();
+setText("https://bisaxa.github.io/p2VerifierLeaderboardBackend/")*/
+var output = ""
+
+let xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+		if (xhr.readyState == XMLHttpRequest.DONE) {
+				output.text = xhr.responseText;
+		}
+		output = xhr.responseText;
+}
+xhr.open("GET", "https://bisaxa.github.io/p2VerifierLeaderboardBackend/" + ((/\?/).test("https://bisaxa.github.io/p2VerifierLeaderboardBackend/") ? "&" : "?") + (new Date()).getTime(), false);
+xhr.send();
+
 //var txtInput = fetch('https://bisaxa.github.io/p2VerifierLeaderboardBackend/')
 //  .then(response => response.text())
 //  .then(text => document.getElementById("table").innerHTML = text)
-document.getElementById("table").innerHTML = mainText;
+document.getElementById("table").innerHTML = output;
