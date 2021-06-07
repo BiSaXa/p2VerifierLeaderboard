6152d36f7a6d6d69f2be484ae5ca17fb604f55c6
Arda Serdar Pektezol <info@ardapektezol.com>
2021-06-07 13:13:54 +0300

    auto-update

---
+++ pushcommit.sh
@@ -0,0 +1,9 @@
+cd /home/bisaxa/Documents/GitHub/p2VerifierLeaderboard
+node getStatsP2.js
+node getStatsP2CE.js
+node getStatsP2SM.js
+node getStatsPR.js
+git checkout gh-pages
+git add .
+git commit -m auto-update
+git push
