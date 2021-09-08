63c519f98be94de754bf300dfd3033703c1797ff
Arda Serdar Pektezol <info@ardapektezol.com>
2021-09-08 20:09:58 +0300

    add ex-verifiers to the list

--- showStats.js
+++ showStats.js
@@ -1,82 +1,82 @@
 function swap(json) {
-			var ret = {};
-			for (var key in json) {
-				ret[json[key]] = key;
-			}
-			return ret;
-		}
-		function sortObject(obj) {
-			return Object.keys(obj)
-			.sort().reduce((a, v) => {
-				a[v] = obj[v];
-				return a;
-			}, {});
-		}
-		function reverseObject(object) {
-			var newObject = {};
-			var keys = [];
-			for (var key in object) {
-				keys.push(key);
-			}
-			for (var i = keys.length - 1; i >= 0; i--) {
-				var value = object[keys[i]];
-				newObject[keys[i]]= value;
-			}
-			return newObject;
-		}
-		let xhr = new XMLHttpRequest();
-		xhr.open("GET", "https://www.speedrun.com/api/v1/games/om1mw4d2", false);
-		xhr.send();
-		moderators = []
-		runsVerified = []
-		database = JSON.parse(xhr.responseText);
-		for (key in database["data"]["moderators"]) {
-			moderators.push(key)
-		}
-		let xhr2 = new XMLHttpRequest();
-		for (var i = moderators.length - 1; i >= 0; i--) {
-			xhr2.open("GET", "https://www.speedrun.com/api/v1/runs?game=om1mw4d2&max=200&examiner=" + moderators[i], false);
+	var ret = {};
+	for (var key in json) {
+		ret[json[key]] = key;
+	}
+	return ret;
+}
+function sortObject(obj) {
+	return Object.keys(obj)
+		.sort().reduce((a, v) => {
+			a[v] = obj[v];
+			return a;
+		}, {});
+}
+function reverseObject(object) {
+	var newObject = {};
+	var keys = [];
+	for (var key in object) {
+		keys.push(key);
+	}
+	for (var i = keys.length - 1; i >= 0; i--) {
+		var value = object[keys[i]];
+		newObject[keys[i]] = value;
+	}
+	return newObject;
+}
+let xhr = new XMLHttpRequest();
+xhr.open("GET", "https://www.speedrun.com/api/v1/games/om1mw4d2", false);
+xhr.send();
+moderators = []
+runsVerified = []
+database = JSON.parse(xhr.responseText);
+for (key in database["data"]["moderators"]) {
+	moderators.push(key)
+}
+let xhr2 = new XMLHttpRequest();
+for (var i = moderators.length - 1; i >= 0; i--) {
+	xhr2.open("GET", "https://www.speedrun.com/api/v1/runs?game=om1mw4d2&max=200&examiner=" + moderators[i], false);
+	xhr2.send();
+	runs = JSON.parse(xhr2.responseText)
+	runsVerifiedNumberP2 = runs.pagination.size
+	do {
+		if (runs.pagination.size == 200) {
+			xhr2.open("GET", "https://www.speedrun.com/api/v1/runs?game=om1mw4d2&max=200&examiner=" + moderators[i] + "&offset=" + runsVerifiedNumberP2, false);
 			xhr2.send();
 			runs = JSON.parse(xhr2.responseText)
-			runsVerifiedNumberP2 = runs.pagination.size
-			do {
-				if (runs.pagination.size == 200) {
-					xhr2.open("GET", "https://www.speedrun.com/api/v1/runs?game=om1mw4d2&max=200&examiner=" + moderators[i] + "&offset=" + runsVerifiedNumberP2, false);
-					xhr2.send();
-					runs = JSON.parse(xhr2.responseText)
-					runsVerifiedNumberP2 += runs.pagination.size
-				}
-
-			} while (runs.pagination.size == 200)
-
-			runsVerified.push(runsVerifiedNumberP2)
-
+			runsVerifiedNumberP2 += runs.pagination.size
 		}
 
-		runsVerified = runsVerified.reverse()
-		var result = {};
-		moderators.forEach((key, i) => result[key] = runsVerified[i]);
+	} while (runs.pagination.size == 200)
 
-		result = swap(result)
-		result = sortObject(result)
-		result = swap(result)
-		result = reverseObject(result)
+	runsVerified.push(runsVerifiedNumberP2)
 
-		let xhr3 = new XMLHttpRequest();
-		var output = ""
-		var txtout = ""
-		var place = 0
-		for (key in result) {
-			place++
-			xhr3.open("GET", "https://www.speedrun.com/api/v1/users/" + key, false);
-			xhr3.send();
-			ModName = JSON.parse(xhr3.responseText)
-			name2 = ModName["data"]["names"]["international"]
-			output += "<tr><td>"+place+"</td><td>" + name2 + "</td><td>" + result[key] + "</td><tr>"
-		}
-		document.getElementById("table").innerHTML = output
+}
 
-window.onload = function() {
+runsVerified = runsVerified.reverse()
+var result = {};
+moderators.forEach((key, i) => result[key] = runsVerified[i]);
+
+result = swap(result)
+result = sortObject(result)
+result = swap(result)
+result = reverseObject(result)
+
+let xhr3 = new XMLHttpRequest();
+var output = ""
+var txtout = ""
+var place = 0
+for (key in result) {
+	place++
+	xhr3.open("GET", "https://www.speedrun.com/api/v1/users/" + key, false);
+	xhr3.send();
+	ModName = JSON.parse(xhr3.responseText)
+	name2 = ModName["data"]["names"]["international"]
+	output += "<tr><td>" + place + "</td><td>" + name2 + "</td><td>" + result[key] + "</td><tr>"
+}
+document.getElementById("table").innerHTML = output
+
+window.onload = function () {
 
 	let p2Board = document.querySelector('#P2Table');
 	p2Board.onclick = () => {
@@ -89,10 +89,10 @@ window.onload = function() {
 		}
 		function sortObject(obj) {
 			return Object.keys(obj)
-			.sort().reduce((a, v) => {
-				a[v] = obj[v];
-				return a;
-			}, {});
+				.sort().reduce((a, v) => {
+					a[v] = obj[v];
+					return a;
+				}, {});
 		}
 		function reverseObject(object) {
 			var newObject = {};
@@ -102,7 +102,7 @@ window.onload = function() {
 			}
 			for (var i = keys.length - 1; i >= 0; i--) {
 				var value = object[keys[i]];
-				newObject[keys[i]]= value;
+				newObject[keys[i]] = value;
 			}
 			return newObject;
 		}
@@ -154,7 +154,7 @@ window.onload = function() {
 			xhr3.send();
 			ModName = JSON.parse(xhr3.responseText)
 			name2 = ModName["data"]["names"]["international"]
-			output += "<tr><td>"+place+"</td><td>" + name2 + "</td><td>" + result[key] + "</td><tr>"
+			output += "<tr><td>" + place + "</td><td>" + name2 + "</td><td>" + result[key] + "</td><tr>"
 		}
 		document.getElementById("table").innerHTML = output
 
@@ -171,10 +171,10 @@ window.onload = function() {
 		}
 		function sortObject(obj) {
 			return Object.keys(obj)
-			.sort().reduce((a, v) => {
-				a[v] = obj[v];
-				return a;
-			}, {});
+				.sort().reduce((a, v) => {
+					a[v] = obj[v];
+					return a;
+				}, {});
 		}
 		function reverseObject(object) {
 			var newObject = {};
@@ -184,7 +184,7 @@ window.onload = function() {
 			}
 			for (var i = keys.length - 1; i >= 0; i--) {
 				var value = object[keys[i]];
-				newObject[keys[i]]= value;
+				newObject[keys[i]] = value;
 			}
 			return newObject;
 		}
@@ -236,7 +236,7 @@ window.onload = function() {
 			xhr3.send();
 			ModName = JSON.parse(xhr3.responseText)
 			name2 = ModName["data"]["names"]["international"]
-			output += "<tr><td>"+place+"</td><td>" + name2 + "</td><td>" + result[key] + "</td><tr>"
+			output += "<tr><td>" + place + "</td><td>" + name2 + "</td><td>" + result[key] + "</td><tr>"
 		}
 		document.getElementById("table").innerHTML = output
 
@@ -253,10 +253,10 @@ window.onload = function() {
 		}
 		function sortObject(obj) {
 			return Object.keys(obj)
-			.sort().reduce((a, v) => {
-				a[v] = obj[v];
-				return a;
-			}, {});
+				.sort().reduce((a, v) => {
+					a[v] = obj[v];
+					return a;
+				}, {});
 		}
 		function reverseObject(object) {
 			var newObject = {};
@@ -266,7 +266,7 @@ window.onload = function() {
 			}
 			for (var i = keys.length - 1; i >= 0; i--) {
 				var value = object[keys[i]];
-				newObject[keys[i]]= value;
+				newObject[keys[i]] = value;
 			}
 			return newObject;
 		}
@@ -318,7 +318,7 @@ window.onload = function() {
 			xhr3.send();
 			ModName = JSON.parse(xhr3.responseText)
 			name2 = ModName["data"]["names"]["international"]
-			output += "<tr><td>"+place+"</td><td>" + name2 + "</td><td>" + result[key] + "</td><tr>"
+			output += "<tr><td>" + place + "</td><td>" + name2 + "</td><td>" + result[key] + "</td><tr>"
 		}
 		document.getElementById("table").innerHTML = output
 
@@ -335,10 +335,10 @@ window.onload = function() {
 		}
 		function sortObject(obj) {
 			return Object.keys(obj)
-			.sort().reduce((a, v) => {
-				a[v] = obj[v];
-				return a;
-			}, {});
+				.sort().reduce((a, v) => {
+					a[v] = obj[v];
+					return a;
+				}, {});
 		}
 		function reverseObject(object) {
 			var newObject = {};
@@ -348,7 +348,7 @@ window.onload = function() {
 			}
 			for (var i = keys.length - 1; i >= 0; i--) {
 				var value = object[keys[i]];
-				newObject[keys[i]]= value;
+				newObject[keys[i]] = value;
 			}
 			return newObject;
 		}
@@ -400,7 +400,7 @@ window.onload = function() {
 			xhr3.send();
 			ModName = JSON.parse(xhr3.responseText)
 			name2 = ModName["data"]["names"]["international"]
-			output += "<tr><td>"+place+"</td><td>" + name2 + "</td><td>" + result[key] + "</td><tr>"
+			output += "<tr><td>" + place + "</td><td>" + name2 + "</td><td>" + result[key] + "</td><tr>"
 		}
 		document.getElementById("table").innerHTML = output
 
